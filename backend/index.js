4d7246facbd66c59964c112781ef61c777fe1897
Pratik Chandra Das <pratikdassonu@gmail.com>
2025-12-23 22:30:13 +0530

    Update CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://realtimetalk-frontend.onrender.com",
     credentials: true,
   })
 );
