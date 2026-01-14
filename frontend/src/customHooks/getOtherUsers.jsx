323b367ec08c178062c75590d30538201fa90885
Pratik Chandra Das <pratikdassonu@gmail.com>
2026-01-14 21:34:51 +0530

    Refactor getOtherUsers to include userData check
    Refactor getOtherUsers hook to check for userData before fetching.

--- frontend/src/customHooks/getOtherUsers.jsx
+++ frontend/src/customHooks/getOtherUsers.jsx
@@ -1,24 +1,30 @@
 import axios from "axios"
 import { useEffect } from "react"
-import { serverUrl } from "../main"
 import { useDispatch, useSelector } from "react-redux"
-import { setOtherUsers, setUserData } from "../redux/userSlice"
+import { serverUrl } from "../main"
+import { setOtherUsers } from "../redux/userSlice"
 
-const getOtherUsers = ()=>{
-    let dispatch = useDispatch()
-    let {userData} = useSelector(state=>state.user)
-    useEffect(()=>{
-        const fetchUser = async () =>{
-            try {
-                let result = await axios.get(`${serverUrl}/api/user/others`, {withCredentials:true})
-                dispatch(setOtherUsers(result.data))
-            } catch (error) {
-                console.log(error)
-                
-            }
-        }
-        fetchUser()
-    },[userData])
+const getOtherUsers = () => {
+  const dispatch = useDispatch()
+  const { userData } = useSelector(state => state.user)
+
+  useEffect(() => {
+    if (!userData) return
+
+    const fetchUsers = async () => {
+      try {
+        const res = await axios.get(
+          `${serverUrl}/api/user/others`,
+          { withCredentials: true }
+        )
+        dispatch(setOtherUsers(res.data))
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    fetchUsers()
+  }, [userData, dispatch])
 }
 
 export default getOtherUsers
