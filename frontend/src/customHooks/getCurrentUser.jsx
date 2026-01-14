f9a5079485b733e5d8a954678941e959337fe14c
Pratik Chandra Das <pratikdassonu@gmail.com>
2026-01-14 21:33:53 +0530

    Refactor getCurrentUser hook
    Refactor getCurrentUser hook to improve error handling and dependency management.

--- frontend/src/customHooks/getCurrentUser.jsx
+++ frontend/src/customHooks/getCurrentUser.jsx
@@ -1,24 +1,28 @@
 import axios from "axios"
 import { useEffect } from "react"
+import { useDispatch } from "react-redux"
 import { serverUrl } from "../main"
-import { useDispatch, useSelector } from "react-redux"
 import { setUserData } from "../redux/userSlice"
 
-const getCurrentUser = ()=>{
-    let dispatch = useDispatch()
-    let {userData} = useSelector(state=>state.user)
-    useEffect(()=>{
-        const fetchUser = async () =>{
-            try {
-                let result = await axios.get(`${serverUrl}/api/user/current`, {withCredentials:true})
-                dispatch(setUserData(result.data))
-            } catch (error) {
-                console.log(error)
-                
-            }
-        }
-        fetchUser()
-    },[])
+const getCurrentUser = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await axios.get(
+          `${serverUrl}/api/user/current`,
+          { withCredentials: true }
+        )
+        dispatch(setUserData(res.data))
+      } catch (error) {
+        // user not logged in → ignore
+        console.log("No current user")
+      }
+    }
+
+    fetchUser()
+  }, [dispatch])
 }
 
 export default getCurrentUser
