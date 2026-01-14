95f1ae0b3b5c4f893cf139844a239852473cebf7
Pratik Chandra Das <pratikdassonu@gmail.com>
2026-01-14 21:36:19 +0530

    Improve getMessages hook with user ID check
    Refactor getMessages hook to check for selectedUser before fetching messages.

--- frontend/src/customHooks/getMessages.jsx
+++ frontend/src/customHooks/getMessages.jsx
@@ -1,26 +1,30 @@
 import axios from "axios"
 import { useEffect } from "react"
-import { serverUrl } from "../main"
 import { useDispatch, useSelector } from "react-redux"
-import { setOtherUsers, setUserData } from "../redux/userSlice"
+import { serverUrl } from "../main"
 import { setMessages } from "../redux/messageSlice"
 
-const getMessages = ()=>{
-    let dispatch = useDispatch()
-    let {userData, selectedUser} = useSelector(state=>state.user)
-    useEffect(()=>{
-        const fetchMessages = async () =>{
-            try {
-                let result = await axios.get(`${serverUrl}/api/message/get/${selectedUser._id}`, 
-                {withCredentials:true})
-                dispatch(setMessages(result.data))
-            } catch (error) {
-                console.log(error)
-                
-            }
-        }
-        fetchMessages()
-    },[selectedUser, userData])
+const getMessages = () => {
+  const dispatch = useDispatch()
+  const { selectedUser } = useSelector(state => state.user)
+
+  useEffect(() => {
+    if (!selectedUser?._id) return
+
+    const fetchMessages = async () => {
+      try {
+        const res = await axios.get(
+          `${serverUrl}/api/message/get/${selectedUser._id}`,
+          { withCredentials: true }
+        )
+        dispatch(setMessages(res.data))
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    fetchMessages()
+  }, [selectedUser, dispatch])
 }
 
 export default getMessages
